932dd20cd2b1f80d4d2bb01b7c2893e9da473308
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-10 19:38:55 +0530

    post OPPE blues

--- question7.py
+++ question7.py
@@ -1,6 +1,7 @@
 a = str (input())
 
 # list_a = list (a)
+len_str_a = len(a)
 
 space = ' '
 
@@ -14,4 +15,13 @@ space = ' '
 #         space_index = i
 #         new_list = list_a.replace(' ', space_index)
 # print(new_list)
+
+for i in range (len_str_a):
+    if (space in a):
+        int_a = str(a.index(space))
+        new_string = a.replace(' ',int_a)
+
+print (new_string)
+
+# the output -> he22llo2bho2go2baan
 
