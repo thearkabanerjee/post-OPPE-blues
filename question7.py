6e0447d677b69348d2a7e3a6f30e879bbf295f2a
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-10 19:43:12 +0530

    post OPPE blues

--- question7.py
+++ question7.py
@@ -1,27 +1,27 @@
-a = str (input())
+# a = str (input())
 
-# list_a = list (a)
-len_str_a = len(a)
+# # list_a = list (a)
+# len_str_a = len(a)
 
-space = ' '
+# space = ' '
 
-# if (space in s):
-#     # replace space by index 
-#     new_list = list_a[]
+# # if (space in s):
+# #     # replace space by index 
+# #     new_list = list_a[]
 
-# len_list = len(a)
-# for i in range (len_list):
-#     if (list_a[i] == space):
-#         space_index = i
-#         new_list = list_a.replace(' ', space_index)
-# print(new_list)
+# # len_list = len(a)
+# # for i in range (len_list):
+# #     if (list_a[i] == space):
+# #         space_index = i
+# #         new_list = list_a.replace(' ', space_index)
+# # print(new_list)
 
-for i in range (len_str_a):
-    if (space in a):
-        int_a = str(a.index(space))
-        new_string = a.replace(' ',int_a)
+# for i in range (len_str_a):
+#     if (space in a):
+#         int_a = str(a.index(space))
+#         new_string = a.replace(' ',int_a)
 
-print (new_string)
+# print (new_string)
 
-# the output -> he22llo2bho2go2baan
+# # the output -> he22llo2bho2go2baan
 
