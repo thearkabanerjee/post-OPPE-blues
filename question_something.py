8f58a10dccf2ef27cdd3a22a8ada59134426a979
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-10 12:56:41 +0530

    post OPPE blues

--- question_something.py
+++ question_something.py
@@ -20,15 +20,29 @@
 #     bool - True if all odd indices are alphabets and even indices are digits, else False
 #     '''
     
-# def is_odd_indices_alpha_and_even_indices_digits(string :str) -> bool :
-#     for i in range(len(string)):
-#         if (i % 2 ==0):
-#             digits = '0123456789'
-#             if (string[i] in digits):
-#                 print (True)
-#             else :
-#                 print (False)
+def is_odd_indices_alpha_and_even_indices_digits(string :str) -> bool :
+    for i in range(len(string)):
+        if (i % 2 ==0):
+            digits = '0123456789'
+            if (string[i] in digits):
+                return (True)
+            else :
+                return (False)
+        elif (i % 2 != 0):
+            alphas = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            if (string[i] in alphas):
+                return (True)
+            else:
+                return (False)
+
+    if (is_odd_indices_alpha_and_even_indices_digits(string) == True):
+        return True
+    else:
+        return False
+
+a = is_odd_indices_alpha_and_even_indices_digits("1e2l3")
+print (a)
 
 
-# a = is_odd_indices_alpha_and_even_indices_digits("hello")
-# print (a)
+# def is_odd_indices_alpha_and_even_indices_digits(string : str) -> bool:
+#     len_string
