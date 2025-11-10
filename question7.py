88f66fa52172ffe010a2181c32a2990d33833b51
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-10 19:23:59 +0530

    post OPPE blues

---
+++ question7.py
@@ -0,0 +1,17 @@
+a = str (input())
+
+# list_a = list (a)
+
+space = ' '
+
+# if (space in s):
+#     # replace space by index 
+#     new_list = list_a[]
+
+# len_list = len(a)
+# for i in range (len_list):
+#     if (list_a[i] == space):
+#         space_index = i
+#         new_list = list_a.replace(' ', space_index)
+# print(new_list)
+
