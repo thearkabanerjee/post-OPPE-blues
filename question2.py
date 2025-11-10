9d215aa5dc6c85377617329141355b83c0f4a678
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-10 10:53:09 +0530

    post OPPE blues

---
+++ question2.py
@@ -0,0 +1,19 @@
+# Create a dictionary with elements in the list as values and the indices as keys.
+# Given a list of items, create a dictionary with the indices as keys and the items as items.
+
+# template code:
+
+
+# def create_indexed_dict(items: list) -> dict:
+#     '''
+#     Given a list of items, create a dictionary with the indices as keys and the items as items.
+
+#     Args:
+#         items (list): A list of items.
+
+#     Returns:
+#         dict: A dictionary with indices as keys and items as items.
+#     '''
+    
+def create_indexed_dict(items: list) -> dict:
+    
---
+++ question3.py
@@ -0,0 +1,5 @@
+# Find Manhattan distance between two 2d points through a point
+# Given three points a, b, and c on the Cartesian plane, calculate the Manhattan distance to go from point a to point c via point b.
+
+# Manhattan distance is the sum of the absolute differences of their Cartesian coordinates.
+
